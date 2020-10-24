5e64fcad672509c1c8f1c77e24a69b2bf1b80fe6
chen <chenjp1989@gmail.com>
2020-10-24 14:30:58 +1100

    Update Homepage.jsx

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import styled from "styled-components";
+
+import Canva from "../Canva";
+import Clip from "../Canva/Clip";
+import SearchBar from "../SearchBar";
+
+const Background = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: #e3e8ec;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  width: 80%;
+  max-width: 1400px;
+  min-width: 1200px;
+  min-height: 700px;
+  max-height: 900px;
+  background-color: #fff;
+  border-radius: 30px;
+  padding: 40px;
+  position: relative;
+  overflow: hidden;
+`;
 
 function HomePage() {
-  return <div></div>;
+  return (
+    <Background>
+      <SearchBar />
+      <Container>
+        <Clip />
+        <Canva />
+      </Container>
+    </Background>
+  );
 }
 
 export default HomePage;
