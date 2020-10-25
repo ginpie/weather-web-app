553db8c3992a350e68d952721e74aebfdde4ca89
chen <chenjp1989@gmail.com>
2020-10-26 00:31:33 +1100

    Update getWeatherByCity.js

--- src/apis/getWeatherByCity/getWeatherByCity.js
+++ src/apis/getWeatherByCity/getWeatherByCity.js
@@ -1,12 +1,20 @@
-const getWeatherByCity = ({ city }) =>
-  fetch(process.env.REACT_APP_, {
-    method: "GET",
-    body: JSON.stringify({
-      city,
-    }),
-    headers: {
-      "content-type": "application/json",
-    },
-  });
+import { REACT_APP_API_CURRENT, REACT_APP_API_APPID } from "../apikey";
+
+const axios = require("axios");
+
+const instance = axios.create({
+  baseURL: REACT_APP_API_CURRENT + "?appid=" + REACT_APP_API_APPID,
+});
+
+function getWeatherByCity(city) {
+  return instance
+    .get("", {
+      params: {
+        q: city,
+      },
+    })
+    .then((res) => res.data)
+    .catch((err) => console.log(err));
+}
 
 export default getWeatherByCity;
