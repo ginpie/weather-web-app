0747f6d6c2bba2da24b7a2b26701c7ead4a4f4dd
chen <chenjp1989@gmail.com>
2020-10-27 00:37:16 +1100

    connect weather icons

---
+++ src/apis/weatherIcon.js
@@ -0,0 +1,19 @@
+const weatherIcon = new Map();
+
+weatherIcon.set(
+  "Clear",
+  "https://res.cloudinary.com/dr99oorie/image/upload/v1603517457/weather-app%20assets/clear_szhil3.svg"
+);
+weatherIcon.set(
+  "Rain",
+  "https://res.cloudinary.com/dr99oorie/image/upload/v1603517503/weather-app%20assets/rain_isnziw.svg"
+);
+weatherIcon.set(
+  "Clouds",
+  "https://res.cloudinary.com/dr99oorie/image/upload/v1603428277/weather-app%20assets/iconfinder_weather-02_1530391_lqsvis.svg"
+);
+weatherIcon.set(
+  "Mist",
+  "https://res.cloudinary.com/dr99oorie/image/upload/v1603708766/weather-app%20assets/mist_xiprlr.svg"
+);
+export default weatherIcon;
