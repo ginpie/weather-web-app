d672d75a2ec21fee92b8ec708f867fd3b1654357
chen <chenjp1989@gmail.com>
2020-10-27 00:39:19 +1100

    add new city-add card

---
+++ src/components/Canva/Others/CardAdd/CardAdd.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+class CardAdd extends React.Component {
+  render() {
+    return <Container></Container>;
+  }
+}
+
+export default CardAdd;
